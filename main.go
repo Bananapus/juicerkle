48c83872634157ceaf0d675e010f06062b58c488
Filip <f@filip.world>
2024-03-20 12:05:58 -0400

    basics working

--- main.go
+++ main.go
@@ -76,20 +76,6 @@ var networks = []struct {
 	},
 }
 
-var suckers = []struct {
-	chainId chainId
-	address common.Address
-}{
-	{
-		chainId: sepolia,
-		address: common.HexToAddress("0xf19736127f87569423eC4F10FD8CC984bbcE0A17"),
-	},
-	{
-		chainId: opSepolia,
-		address: common.HexToAddress("0xf19736127f87569423eC4F10FD8CC984bbcE0A17"),
-	},
-}
-
 func main() {
 	// Set up ETH clients
 	for _, network := range networks {
@@ -154,62 +140,19 @@ func proof(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 
-		tree, err := localSucker.Inbox(&bind.CallOpts{Context: ctx}, toProve.Token)
+		inboxTree, err := localSucker.Inbox(&bind.CallOpts{Context: ctx}, toProve.Token)
 		if err != nil {
 			log.Printf("Failed to get tree: %v\n", err)
 			http.Error(w, "Failed to get tree", http.StatusInternalServerError)
 			return
 		}
 
-		// Check if the inbox root + count matches the database
-		var dbRoot string
-		var dbCount uint
-		err = db.QueryRowContext(ctx, `SELECT current_root, count FROM trees
-			WHERE chain_id = ? AND contract_address = ? AND token_address = ?`,
-			toProve.ChainId, toProve.Sucker.String(), toProve.Token.String()).Scan(&dbRoot, &dbCount)
-
-		if err != nil && err != sql.ErrNoRows {
-			log.Printf("Failed to query database: %v\n", err)
-			http.Error(w, "Failed to query database", http.StatusInternalServerError)
-			return
-		}
-
-		// If the database did not have the tree, or if the roots didn't match, update the database.
-		if err == sql.ErrNoRows || dbRoot != hex.EncodeToString(tree.Root[:]) {
-			// Update the database
-			updateLeaves(ctx, TokenTree{
-				ChainId:       chainId(toProve.ChainId),
-				SuckerAddress: toProve.Sucker,
-				TokenAddress:  toProve.Token,
-				Root:          tree.Root,
-			})
-		}
-
-		// If it does, get the proof from the database and return it
-
-		// If it doesn't, update the database and get the proof from the sucker
-
-		peerChainId, peerContractAddr, err := peer(toProve.ChainId, toProve.Sucker, localSucker, ctx)
-		if err != nil {
-			http.Error(w, "Failed to get peer", http.StatusInternalServerError)
-			return
-		}
-
-		peerClient, ok := clients[peerChainId]
-		if !ok {
-			log.Printf("Peer chain %d not supported\n", peerChainId)
-			http.Error(w, "Peer chain not supported", http.StatusBadRequest)
-			return
-		}
-
-		remoteSucker, err := NewBPSucker(peerContractAddr, peerClient)
-		if err != nil {
-			log.Printf("Failed to instantiate the peer sucker contract: %v\n", err)
-			http.Error(w, "Failed to instantiate the peer sucker contract", http.StatusInternalServerError)
-			return
-		}
-
-		proof, err := suckerProof(remoteSucker, toProve.Leaf, ctx)
+		proof, err := dbProof(ctx, toProve.Leaf, InboxTree{
+			ChainId:       toProve.ChainId,
+			SuckerAddress: toProve.Sucker,
+			TokenAddress:  toProve.Token,
+			Root:          inboxTree.Root,
+		})
 		if err != nil {
 			log.Printf("Failed to get proof: %v\n", err)
 			http.Error(w, "Failed to get proof", http.StatusInternalServerError)
@@ -247,7 +190,7 @@ type Leaf struct {
 }
 
 // Update the leaves in the database for a specific sucker
-func updateLeaves(ctx context.Context, tokenTree TokenTree) error {
+func updateLeaves(ctx context.Context, tokenTree InboxTree) error {
 	client, ok := clients[tokenTree.ChainId]
 	if !ok {
 		return fmt.Errorf("chain %d not supported", tokenTree.ChainId)
@@ -268,20 +211,21 @@ func updateLeaves(ctx context.Context, tokenTree TokenTree) error {
 		return fmt.Errorf("failed to query database: %v", err)
 	}
 
-	latestHashBytes, err := hex.DecodeString(latestHash)
-	if err != nil {
-		return fmt.Errorf("failed to decode leaf hash '%s': %v", latestHash, err)
-	}
-
 	seenLatestDbLeaf := false
+	var latestHashBytes []byte
 	// Start from the beginning if there are no leaves in the db
 	if err == sql.ErrNoRows {
 		seenLatestDbLeaf = true
+	} else {
+		if latestHashBytes, err = hex.DecodeString(latestHash); err != nil {
+			return fmt.Errorf("failed to decode leaf hash '%s': %v", latestHash, err)
+		}
 	}
 
 	inboxTreeRoot, err := sucker.Inbox(&bind.CallOpts{Context: ctx}, tokenTree.TokenAddress)
 	if err != nil {
-		return fmt.Errorf("failed to get inbox tree root: %v", err)
+		return fmt.Errorf("failed to get inbox tree root for token '%s': %v",
+			tokenTree.TokenAddress.String(), err)
 	}
 
 	peerSuckerAddr, err := sucker.PEER(&bind.CallOpts{Context: ctx})
@@ -290,11 +234,12 @@ func updateLeaves(ctx context.Context, tokenTree TokenTree) error {
 	}
 
 	var peerChainId chainId
-	if tokenTree.ChainId == sepolia {
+	switch tokenTree.ChainId {
+	case sepolia:
 		peerChainId = opSepolia
-	} else if tokenTree.ChainId == opSepolia {
+	case opSepolia:
 		peerChainId = sepolia
-	} else {
+	default:
 		return fmt.Errorf("peer chain not supported")
 	}
 
@@ -305,7 +250,7 @@ func updateLeaves(ctx context.Context, tokenTree TokenTree) error {
 
 	peerSucker, err := NewBPSucker(peerSuckerAddr, peerClient)
 	if err != nil {
-		return fmt.Errorf("failed to instantiate the peer sucker contract: %v", err)
+		return fmt.Errorf("failed to instantiate the peer sucker contract '%s' on chain %d: %v", peerSuckerAddr.String(), peerChainId, err)
 	}
 
 	outboxIterator, err := peerSucker.FilterInsertToOutboxTree(&bind.FilterOpts{Context: ctx}, nil, []common.Address{tokenTree.TokenAddress})
@@ -396,132 +341,73 @@ func (leaf BPLeaf) hash() (common.Hash, error) {
 	return common.BytesToHash(hash), nil
 }
 
-// Get the peer chain ID and contract address for a sucker
-func peer(suckerChainId chainId, suckerAddress common.Address, localSucker *BPSucker, ctx context.Context) (chainId, common.Address, error) {
-	var dbPeerChainId int
-	var dbPeerContractAddr string
-
-	// Check the db for the sucker contract's peer
-	err := db.QueryRowContext(ctx, `SELECT peer_chain_id, peer_contract_address FROM suckers
-		WHERE chain_id = ? AND contract_address = ?`,
-		suckerChainId, suckerAddress.String()).Scan(&dbPeerChainId, &dbPeerContractAddr)
-
-	select {
-	case <-ctx.Done():
-		log.Printf("Request cancelled: %v\n", ctx.Err())
-		return 0, common.Address{}, fmt.Errorf("request cancelled")
-	default:
-		// Continue
-	}
-
-	// If the sucker was found in the db
-	if err == nil {
-		// Convert the results to the correct types and return
-		return chainId(dbPeerChainId), common.HexToAddress(dbPeerContractAddr), nil
-	} else if err == sql.ErrNoRows {
-		// If the sucker wasn't found in the db, we have to read the data from the blockchain
-		// TODO: Read the peer chain ID from the sucker. Mocking for now
-		peerChainId := chainId(1)
-
-		peerContractAddr, err := localSucker.PEER(&bind.CallOpts{Context: ctx})
-		if err != nil {
-			log.Printf("Failed to get peer from sucker: %v\n", err)
-			return 0, common.Address{}, fmt.Errorf("failed to get peer from sucker")
-		}
-
-		// Store the sucker in the database for next time.
-		db.Exec(`INSERT INTO suckers (?, ?, ?, ?)`,
-			suckerChainId, suckerAddress,
-			int(peerChainId), peerContractAddr.String(),
-		)
-
-		return peerChainId, peerContractAddr, nil
-	} else {
-		// TODO: Should we remove this case and read from the blockchain if the db has an error?
-		return 0, common.Address{}, fmt.Errorf("failed to query database")
-	}
-}
-
-type TokenTree struct {
+// A specification for an inbox tree on a sucker contract
+type InboxTree struct {
 	ChainId       chainId
 	SuckerAddress common.Address
 	TokenAddress  common.Address
 	Root          common.Hash
-	Count         *big.Int
 }
 
-func dbProof(leaf BPLeaf, tokenTree *TokenTree, ctx context.Context) ([][]byte, bool, error) {
+// Get the proof for a leaf from the database
+func dbProof(ctx context.Context, leaf BPLeaf, inboxTree InboxTree) ([][]byte, error) {
 	var dbRoot string
-	var dbCount uint
+	var err error
 
-	err := db.QueryRowContext(ctx, `SELECT current_root, count FROM trees
+	if err = db.QueryRowContext(ctx, `SELECT current_root FROM trees
 		WHERE chain_id = ? AND contract_address = ? AND token_address = ?`,
-		tokenTree.ChainId, tokenTree.SuckerAddress.String(), tokenTree.TokenAddress.String()).
-		Scan(&dbRoot, &dbCount)
-
-	// TODO: this probably doesn't matter that much
-	select {
-	case <-ctx.Done():
-		log.Printf("Request cancelled: %v\n", ctx.Err())
-		return nil, false, fmt.Errorf("request cancelled")
-	default:
-		// Continue
-	}
-
-	if err != nil && err != sql.ErrNoRows {
-		return nil, false, fmt.Errorf("failed to query database")
+		inboxTree.ChainId, inboxTree.SuckerAddress.String(), inboxTree.TokenAddress.String()).
+		Scan(&dbRoot); err != nil && err != sql.ErrNoRows {
+		return nil, fmt.Errorf("failed to query database")
 	}
 
 	// If no rows were found, or the counts/roots don't match, we need to update the database.
-	if err == sql.ErrNoRows || dbRoot != tokenTree.Root.String() || tokenTree.Count.Cmp(big.NewInt(int64(dbCount))) != 0 {
-		// TODO: update the database
+	if err == sql.ErrNoRows || dbRoot != inboxTree.Root.String() {
+		if err := updateLeaves(ctx, inboxTree); err != nil {
+			return nil, fmt.Errorf("failed to update leaves: %v", err)
+		}
 	}
 
 	rows, err := db.QueryContext(ctx, `SELECT leaf_hash FROM leaves
 		WHERE chain_id = ? AND contract_address = ? AND token_address = ?`,
-		tokenTree.ChainId, tokenTree.SuckerAddress.String(), tokenTree.TokenAddress.String())
+		inboxTree.ChainId, inboxTree.SuckerAddress.String(), inboxTree.TokenAddress.String())
 	if err != nil {
-		if err == sql.ErrNoRows {
-			// TODO: update the database
-		} else {
-			return nil, false, fmt.Errorf("failed to query database")
-		}
+		// Note: this includes sql.ErrNoRows
+		return nil, fmt.Errorf("failed to get '%d:%s:%s' leaves from the database: %v",
+			inboxTree.ChainId, inboxTree.SuckerAddress.String(), inboxTree.TokenAddress.String(), err)
 	}
 
-	leaves := make([][]byte, 0, dbCount)
+	leaves := make([][]byte, 0)
 	for rows.Next() {
 		var leafHash string
 		err := rows.Scan(&leafHash)
 		if err != nil {
-			return nil, false, fmt.Errorf("failed to scan leaf hash %s: %v", leafHash, err)
+			return nil, fmt.Errorf("failed to scan leaf hash %s: %v", leafHash, err)
 		}
 
 		b, err := hex.DecodeString(leafHash)
 		if err != nil {
-			return nil, false, fmt.Errorf("failed to decode leaf hash %s: %v", leafHash, err)
+			return nil, fmt.Errorf("failed to decode leaf hash %s: %v", leafHash, err)
 		}
 
 		leaves = append(leaves, b)
 	}
 
+	// Construct a tree with the retrieved leaves
 	t := tree.NewTree(leaves)
 	treeRoot := t.Root()
 
 	// Sanity check the tree root
-	if !slices.Equal(treeRoot, tokenTree.Root.Bytes()) {
-		// TODO: update the database
-		return nil, false, fmt.Errorf("calculated tree root does not match reported tree root in db")
+	if !slices.Equal(treeRoot, inboxTree.Root.Bytes()) {
+		return nil, fmt.Errorf("calculated tree root '%s' does not match expected tree root '%s'",
+			hex.EncodeToString(treeRoot), inboxTree.Root.String())
 	}
 
 	// Bounds are checked in the proof function
 	p, err := t.Proof(uint(leaf.Index.Uint64()))
 	if err != nil {
-		return nil, false, fmt.Errorf("failed to get proof: %v", err)
+		return nil, fmt.Errorf("failed to get proof: %v", err)
 	}
 
-	return p, true, nil
-}
-
-func suckerProof(remoteSucker *BPSucker, leaf BPLeaf, ctx context.Context) ([][]byte, error) {
-	return nil, nil
+	return p, nil
 }
